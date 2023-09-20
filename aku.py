1c7776057d637e025645e82aad288dae027af38e
piannnnnn <138741452+piannnnnn@users.noreply.github.com>
2023-09-20 14:33:24 +0700

    Add files via upload

---
+++ aku.py
@@ -0,0 +1,21 @@
+print("===== Hello Word =====")
+print("===== Selamat Belajar Python =====")
+nama = input("Nama ")
+print(nama)
+alamat = input("Alamat ")
+print(alamat)
+hobi = input("Hobi ")
+print(hobi)
+
+
+print("===== BIODATA =====")
+
+
+
+print("halo perkenalkan nama saya {} ,saya saat ini tinggal di {}  dan hobi saya adalah {} "\
+    .format(nama, alamat, hobi, ))
+print("SENANG BISA BERKENALAN DENGANMU!!")
+print("dalam 3 bulan kedepan saya akan belajar Python")
+print("")
+
+
